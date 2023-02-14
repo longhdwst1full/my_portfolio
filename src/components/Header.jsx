dc8c4a36fd464a6ef6c9d5aa26b8f6d00ac9cc11
long1703@outlook.com <long1703@outlook.com>
2023-02-14 16:06:38 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { navDetail } from "../api/data";
+import { useEffect, useState } from "react";
+import { intance } from "../api";
 const Header = ({ children }) => {
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    intance
+      .get("/navDetail")
+      .then(({ data }) => data)
+      .then((data) => setMenu(data));
+  }, []);
+
   return (
     <div className="fixed top-0 left-0 right-0 py-4  font-bold bg-white text-black w-full  m-auto shadow-[#92a1b04d] z-40">
       {children}
@@ -9,16 +17,17 @@ const Header = ({ children }) => {
           <a href=""> HDL</a>
         </div>
         <nav className="flex list-none ">
-          {navDetail.map((item) => (
-            <li key={item.name} className="ml-6">
-              <a
-                className="py-1.5 text-[#0e2431] text-lg font-semibold tracking-[0.04rem]  "
-                href="#"
-              >
-                {item.name}
-              </a>
-            </li>
-          ))}
+          {menu.length  &&
+            menu.map((item) => (
+              <li key={item.name} className="ml-6">
+                <a
+                  className="py-1.5 text-[#0e2431] text-lg font-semibold tracking-[0.04rem]  "
+                  href={item.path}
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
         </nav>
       </div>
     </div>
